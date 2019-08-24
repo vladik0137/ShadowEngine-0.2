b645c7e0c6a8da279a9d4e69f869b983cd84644c
justin sanders <swordsmaster1991@gmail.com>
2019-08-24 11:29:07 -0500

    Icremented a pause function using chrono, not tested yet

--- ShadowEngine/GameTimer.cpp
+++ ShadowEngine/GameTimer.cpp
@@ -19,3 +19,37 @@ float GameTimer::Peek() const
 {
 	return duration<float>(steady_clock::now() - last).count();
 }
+
+void GameTimer::Reset()
+{
+}
+
+float GameTimer::Pause()
+{
+	/*When we call pause we need to stop incrementing the watch
+	to do this we need to keep track of the time passed while 
+	we are paused*/
+
+	duration<float> pauseTime;
+
+	//First we need to know if the application is paused
+	if (isPaused = true)
+	{
+		auto pauseStart = last;
+		steady_clock::time_point pauseEnd;
+		while (isPaused = true)
+		{
+			//while we are paused we need to keep track of time elapsed
+			pauseEnd = steady_clock::now();
+		}
+
+		// Once we arent paused we need to subtract that time from the timer
+		pauseTime = pauseEnd - pauseStart;
+	}
+
+	return pauseTime.count;
+}
+
+void GameTimer::Tick()
+{
+}
--- ShadowEngine/GameTimer.h
+++ ShadowEngine/GameTimer.h
@@ -8,6 +8,20 @@ public:
 	GameTimer();
 	float Mark();
 	float Peek() const;
+
+	/* In essence this entire class is an elaberate stop watch
+	that will be used to keep track of frame counts pause time
+	etc*/
+
+	/// All the basic mechanics of our watch that will track time
+	/// calcutalted in Seconds!
+
+	void Reset(); // Clear the stop watch
+	float Pause(); // pause the watch
+	void Tick(); // increment the watch
 private:
+
+	bool isPaused = false;
+
 	std::chrono::steady_clock::time_point last;
 };
